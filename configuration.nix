bbf984c2f5e05368ae11b3a5ffd1ea334ab1bb69
meowkita <meowkita@proton.me>
2026-05-02 16:20:39 +0500

    added fuzzel

--- configuration.nix
+++ configuration.nix
@@ -13,30 +13,24 @@
   networking.firewall.enable = false;
 
   time.timeZone = "Asia/Almaty";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
+  
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # Enable the X11 windowing system.
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
   services.xserver.enable = true;
 
   programs.niri.enable = true;
 
-  # Enable sound.
   # services.pipewire = {
   #   enable = true;
   #   pulse.enable = true;
   # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
 
   users.users.meowkita = {
     isNormalUser = true;
@@ -49,14 +43,9 @@
   environment.systemPackages = with pkgs; [
     git
     wget
+    fuzzel
     alacritty
   ];
-
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   system.stateVersion = "25.11";
 }
