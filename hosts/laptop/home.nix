a19da6aa57cf6a2ddef8f72b6ac1616821c5de24
meowkita <meowkita@proton.me>
2026-05-03 17:53:40 +0500

    add vscode config

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -10,6 +10,10 @@
   home.file.".config/fuzzel/fuzzel.ini".source = ../../configs/fuzzel/fuzzel.ini;
   home.file.".config/alacritty/alacritty.toml".source = ../../configs/alacritty/alacritty.toml;
 
+  home.file.".vscode-oss/extensions.json".source = ../../configs/vscode/extensions.json;
+  home.file.".config/VSCodium/product.json".source = ../../configs/vscode/product.json;
+  home.file.".config/VSCodium/User/settings.json".source = ../../configs/vscode/settings.json;
+
   home.packages = with pkgs; [
     bibata-cursors
     papirus-icon-theme
