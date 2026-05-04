fca84fba80eab083e77c3ce8783bbd4af4acf7f0
meowkita <meowkita@proton.me>
2026-05-04 18:01:40 +0500

    tidy up config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,30 +8,51 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  boot.loader.limine.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.consoleLogLevel = 3;
-
-  networking.hostName = "laptop";
-  networking.networkmanager.enable = true;
-  networking.firewall.enable = false;
-
-  time.timeZone = "Asia/Almaty";
-  i18n.defaultLocale = "en_US.UTF-8";
-  
-  programs.niri.enable = true;
-  programs.xwayland.enable = true;
+  boot = {
+    loader.limine.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    consoleLogLevel = 3;
+  };
 
   hardware.graphics.enable = true;
 
-  services.xserver.enable = true;
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
+  networking = {
+    hostName = "laptop";
+    networkmanager.enable = true;
+    firewall.enable = false;
+  };
+
+  time.timeZone = "Asia/Almaty";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services = {
+    xserver.enable = true;
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+  };
+
+  environment = {
+    variables = {
+      NIXOS_OZONE_WL = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+      OZONE_PLATFORM = "wayland";
+    };
+
+    systemPackages = with pkgs; [
+      git
+      tree
+      wget
+      btop
+      neovim
+      killall
+      brightnessctl
+      xwayland-satellite
+    ];
   };
 
   fonts.packages = with pkgs; [
-    jetbrains-mono
     nerd-fonts.jetbrains-mono
     nerd-fonts.fira-code
   ];
@@ -41,21 +62,10 @@
     extraGroups = [ "wheel" "networkmanager" ];
   };
 
-  environment.variables = {
-    NIXOS_OZONE_WL = "1";
-    MOZ_ENABLE_WAYLAND = "1";
-    OZONE_PLATFORM = "wayland";
+  programs = {
+    niri.enable = true;
+    xwayland.enable = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    git
-    tree
-    wget
-    btop
-    neovim
-    brightnessctl
-    xwayland-satellite
-  ];
 
   system.stateVersion = "25.11";
 }
