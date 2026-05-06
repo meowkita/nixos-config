c392263d27385a03f46af6b5dab1e58b8a8a50b2
meowkita <meowkita@proton.me>
2026-05-06 23:58:19 +0500

    one day copypaste will be defeated...

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -6,6 +6,7 @@
   home.homeDirectory = "/home/meowkita";
   
   home.file = {
+    ".config/mako/config".source = ../../configs/mako/config;
     ".config/swaybg/nms.jpg".source = ../../configs/swaybg/nms.jpg;
     ".config/niri/config.kdl".source = ../../configs/niri/config.kdl;
     ".config/swaylock/config".source = ../../configs/swaylock/config;
@@ -15,6 +16,8 @@
     ".config/VSCodium/product.json".source = ../../configs/vscode/product.json;
     ".config/VSCodium/User/settings.json".source = ../../configs/vscode/settings.json;
     ".config/alacritty/alacritty.toml".source = ../../configs/alacritty/alacritty.toml;
+    ".config/btop/themes/nms.theme".source = ../../configs/btop/themes/nms.theme;
+    ".config/btop/btop.conf".source = ../../configs/btop/btop.conf;
   };
 
   home.packages = with pkgs; [
@@ -50,6 +53,71 @@
     bitwarden-desktop
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
+
+  programs = {
+    bash = {
+      enable = true;
+    };
+
+    git = {
+      enable = true;
+      settings.user = {
+        name = "meowkita";
+        email = "meowkita@proton.me";
+      };
+    };
+
+    starship = {
+      enable = true;
+      enableBashIntegration = true;
+
+      settings = {
+        add_newline = false;
+        format = "$username$hostname$directory$git_branch$git_status$nix_shell$character";
+
+        username = {
+          show_always = true;
+          style_user = "bold #78e7e8";
+          format = "[$user]($style)";
+        };
+
+        hostname = {
+          ssh_only = false;
+          style = "bold #78e7e8";
+          format = "[@$hostname]($style) ";
+        };
+
+        directory = {
+          style = "bold #fff4d6";
+          truncation_length = 3;
+          truncate_to_repo = true;
+          format = "[$path]($style) ";
+        };
+
+        git_branch = {
+          symbol = " ";
+          style = "#ff7a8a";
+          format = "[$symbol$branch]($style) ";
+        };
+
+        git_status = {
+          style = "#ffd166";
+          format = "[$all_status$ahead_behind]($style) ";
+        };
+
+        nix_shell = {
+          symbol = " ";
+          style = "#78e7e8";
+          format = "[$symbol$state]($style) ";
+        };
+
+        character = {
+          success_symbol = "[❯](bold #a6e3a1)";
+          error_symbol = "[❯](bold #ff7a8a)";
+        };
+      };
+    };
+  };
 
   home.pointerCursor = {
     size = 24;
